c687b83a520e98993a0e1e6aff70031b2bccc39e
zht2649825643 <2649825643@qq.com>
2021-05-28 02:48:10 -0700

    2021-new_note

--- gitee.sh
+++ gitee.sh
@@ -1,5 +1,5 @@
 git remote rm origin
-git remote add origin https://gitee.com/zhang-haitao-1/linux_study.git
+git remote add origin https://gitee.com/zhang-haitao-1/mylinuxkernel-imx6ull.git
 git add .
 git commit -m "2021-new_note"
 git push -u origin main
--- github.sh
+++ github.sh
@@ -1,7 +1,7 @@
 git config  --global user.name zht2649825643
 git config  --global user.email 2649825643@qq.com
 git remote rm origin
-git remote add origin https://github.com/zht2649825643/linux_study.git  
+git remote add origin https://github.com/zht2649825643/myLinuxKernel-imx6ull.git 
 git add .
 git commit -m "2021-new_note"
 git push -u origin main
